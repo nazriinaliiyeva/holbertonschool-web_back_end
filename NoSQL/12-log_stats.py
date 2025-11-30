ad8b46e1fc0eaee8223ac9554c2cfff874407402
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-11-30 10:51:30 +0000

    nosql

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -6,7 +6,7 @@ Script that provides some stats about Nginx logs stored in MongoDB
 from pymongo import MongoClient
 
 if __name__ == "__main__":
-    client = MongoClient('mongodb://localhost:27017')
+    client = MongoClient('mongodb://localhost:27017/')
     db = client.logs
     collection = db.nginx
 
@@ -24,4 +24,3 @@ if __name__ == "__main__":
     # Number of GET /status requests
     status_check = collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_check} status check")
-
