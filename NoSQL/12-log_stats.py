1984ef63985baadbad6ecb884019cfac98b6af0b
nazriinaliiyeva <nazriinaliiyeva@gmail.com>
2025-11-30 10:45:11 +0000

    nosql

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""documentation"""
+
+
+from pymango import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://localhost:27017')
+    db = client.logs
+    collection = db.nginx
+
+    total_logs = collection.count_documents({})
+    print(f"{total_logs} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
